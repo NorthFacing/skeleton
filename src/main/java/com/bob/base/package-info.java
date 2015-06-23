35ba6dbaddee245c838191a4ce38d4eae56c0cff
NorthFacing <0haizhu0@gmail.com>
2015-06-23 19:51:12 +0800

    info.java代码注释

--- src/main/java/com/bob/base/package-info.java
+++ src/main/java/com/bob/base/package-info.java
@@ -1,7 +1,7 @@
 /** 
  * @Title: package-info.java 
  * @Package com.bob.base 
- * @Description: TODO
+ * @Description: 基础共用代码封装目录
  * @author Bob
  * @date 2015年6月19日 上午3:20:53   
  */
--- src/main/java/com/bob/bbs/package-info.java
+++ src/main/java/com/bob/bbs/package-info.java
@@ -1,7 +1,7 @@
 /** 
  * @Title: package-info.java 
  * @Package com.bob.demo 
- * @Description: TODO
+ * @Description: 讨论/问答 代码目录
  * @author Bob   
  * @date 2015年6月19日 上午2:13:06  
  */
