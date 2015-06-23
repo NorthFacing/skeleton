ac8ccdafab4f5476a4996c9f12cb6ebd74d3e782
NorthFacing <0haizhu0@gmail.com>
2015-06-24 00:25:07 +0800

    多视图支持解析类

---
+++ src/main/java/com/bob/common/springmvc/package-info.java
@@ -0,0 +1,9 @@
+/** 
+ * @Title: package-info.java 
+ * @Package com.bob.common.springmvc 
+ * @Description: spring mvc 多视图自定义解析器
+ * @author Bob   
+ * @date 2015年6月24日 上午12:14:56  
+ */
+
+package com.bob.common.springmvc;
