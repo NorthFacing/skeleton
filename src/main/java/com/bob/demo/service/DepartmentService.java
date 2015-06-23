200a7e0ca882150434af8de1e8ffb39cfe22152a
NorthFacing <0haizhu0@gmail.com>
2015-06-24 00:44:54 +0800

    VM视图测试代码

---
+++ src/main/java/com/bob/demo/service/DepartmentService.java
@@ -0,0 +1,11 @@
+package com.bob.demo.service;
+
+import java.util.List;
+
+import com.bob.demo.model.Department;
+
+public interface DepartmentService {
+
+    public abstract List<Department> listDepartment();
+
+}
