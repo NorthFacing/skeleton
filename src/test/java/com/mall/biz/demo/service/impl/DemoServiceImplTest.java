8c3f0bca6a0b41fd7ca60b84fa58bb7ecad53ef9
NorthFacing <0haizhu0@gmail.com>
2015-07-16 23:53:02 +0800

    分页方法测试

--- src/test/java/com/mall/biz/demo/service/impl/DemoServiceImplTest.java
+++ src/test/java/com/mall/biz/demo/service/impl/DemoServiceImplTest.java
@@ -9,6 +9,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.github.pagehelper.PageInfo;
 import com.mall.biz.demo.model.Demo;
 import com.mall.biz.demo.service.DemoService;
 import com.mall.core.base.BaseServiceTest;
@@ -60,16 +61,46 @@ public class DemoServiceImplTest extends BaseServiceTest {
     @Test
     public void testGetList() {
 
-        String id = demoService.add(model);
-        Assert.assertNotNull("add 方法返回值为空！", id);
+        int j = 35;
 
-        Demo modelTwo = new Demo();
-        modelTwo.setName(name + name);
-        String idTwo = demoService.add(modelTwo);
-        Assert.assertNotNull("add 方法返回值为空！", idTwo);
+        for (int i = 0; i < j; i++) {
+            model.setName(name + i);
+            String id = demoService.add(model);
+            Assert.assertNotNull("add 方法返回值为空！", id);
+        }
 
-        List<Demo> list = demoService.getList(new Demo().setName(name));
-        Assert.assertTrue("getList 方法 查询结果不正确！", list.size() >= 1);
+        String newName = new StringBuffer(name).reverse().toString();
+        for (int i = 0; i < j; i++) {
+            model.setName(newName + i);
+            String id = demoService.add(model);
+            Assert.assertNotNull("add 方法返回值为空！", id);
+        }
+
+        List<Demo> list01 = demoService.getList(null);
+        Assert.assertTrue("getList 方法 查询结果不正确！", list01.size() >= j * 2);
+
+        List<Demo> list02 = demoService.getList(model);
+        Assert.assertTrue("getList 方法 查询结果不正确！", list02.size() * 2 <= list01.size());
+
+    }
+
+    @Test
+    public void TestgetPage() {
+
+        for (int i = 0; i < 35; i++) {
+            model.setName(name + i);
+            demoService.add(model);
+        }
+
+        List<Demo> list = demoService.getList(null);
+        Assert.assertTrue("getList 方法 查询结果不正确！", list.size() >= 35);
+        
+        int pageNum = 2;
+        int pageSize = 3;
+
+        PageInfo<Demo> page = demoService.getPage(pageNum, pageSize, null);
+        Assert.assertTrue("", page.getList().size() == 3);
+
     }
 
     @Test
@@ -77,13 +108,14 @@ public class DemoServiceImplTest extends BaseServiceTest {
         String id = demoService.add(model);
         Assert.assertNotNull("update 方法返回值为空！", id);
 
-        String newName = new StringBuffer(model.getName()).reverse().toString();
+        String newName = new StringBuffer(name).reverse().toString();
+        model.setName(newName);
         demoService.updateById(model);
 
         Demo result = (Demo) demoService.getById(id);
         Assert.assertNotNull("getById 方法返回值为空！", result);
         Assert.assertNotNull("getById 方法返回值 name 为空！", result.getName());
-        Assert.assertEquals(newName, new StringBuffer(result.getName()).reverse().toString());
+        Assert.assertEquals(name, new StringBuffer(result.getName()).reverse().toString());
     }
 
     @Test
