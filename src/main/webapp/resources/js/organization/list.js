44d872e8bd3229f3b2741a6a6157ab0051c6a78a
NorthFacing <0haizhu0@gmail.com>
2015-08-19 00:34:36 +0800

    rebuild

---
+++ src/main/webapp/resources/js/organization/list.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+	initGrid();
+	// 同步方法，放在后面
+	initZTree();
+});
+
+/** zTree 开始 */
+var setting = {
+	async : {
+		enable : true,
+		autoParam : [ "id" ],
+		url : path + '/admin/organization/getList',
+		contentType : "application/json",
+		dataFilter : ajaxDataFilter,
+		otherParam : {
+			"sidx" : "name",
+			"sord" : "asc"
+		},
+		type : "post"
+	},
+	callback : {
+		onClick : zTreeOnClick
+	},
+	data : {
+		simpleData : {
+			enable : true,
+			idKey : "id",
+			pIdKey : "parentId"
+
+		}
+	}
+};
+
+function zTreeOnClick(event, treeId, treeNode) {
+	search(treeNode.id);
+};
+
+function ajaxDataFilter(treeId, parentNode, responseData) {
+	if (responseData.code = 200) {
+		return responseData.data;
+	} else {
+		JZ.alert("数据加载异常：" + responseData.msg);
+	}
+};
+
+function initZTree() {
+	$.fn.zTree.init($("#orgTree"), setting);
+}
+
+/** zTree 结束 */
+
+/** list 开始 */
+function initGrid() {
+	$("#organizationList").jqGrid({
+		url : path + "/admin/organization/getPage",
+		datatype : "json",
+		colNames : [ 'id','parentId', '层级名称', '层级编码', '层级全称' ],
+		colModel : [ {
+			name : 'id',
+			index : 'id',
+			hidden : true
+		}, {
+			name : 'parentId',
+			index : 'parentId',
+			hidden : true
+		}, {
+			name : 'name',
+			index : 'name',
+			width : 200
+		}, {
+			name : 'code',
+			index : 'code',
+			width : 150
+		}, {
+			name : 'fullName',
+			index : 'fullName',
+			width : 460
+		} ],
+		pager : '#organizationPager'
+	});
+}
+
+function search(parentId) {
+	var postData = $("#organizationList").jqGrid("getGridParam", "postData");
+	var s_name = $('#s_name').val();
+	$.extend(postData, {
+		'name' : s_name,
+		'parentId' : parentId
+	});
+	$("#organizationList").trigger("reloadGrid");
+}
+
+/** list 结束 */
