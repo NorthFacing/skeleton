2d6128c7258d625848372012ad047a582af997e7
NorthFacing <0haizhu0@gmail.com>
2015-08-19 00:36:47 +0800

    delete mall code

--- src/main/java/com/bob/biz/simple/mapper/SimpleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bob.biz.simple.mapper;
-
-import tk.mybatis.mapper.common.Mapper;
-
-import com.bob.biz.simple.model.Simple;
-
-public interface SimpleMapper extends Mapper<Simple> {
-
-}
