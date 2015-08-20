bcba677d1c186144f6997e52e829ea7e7ac0fda0
NorthFacing <0haizhu0@gmail.com>
2015-08-20 14:10:28 +0800

    增加返回结果码表

--- src/main/java/com/bob/core/contants/ResultCode.java
+++ src/main/java/com/bob/core/contants/ResultCode.java
@@ -11,10 +11,13 @@ public class ResultCode {
 
     /** 操作成功 */
     public static final int SUCCESS = 200;
+    public static final String STR_SUCCESS = "SUCCESS";
     /** 操作失败通用码 */
     public static final int FAILED = 444;
-   
-    
+    public static final String STR_FAILED = "FAILED";
+
     /** 参数错误 */
     public static final int ARGS_ERROR = 401;
+    /** 找不到资源 */
+    public static final int NOT_FOUND = 404;
 }
