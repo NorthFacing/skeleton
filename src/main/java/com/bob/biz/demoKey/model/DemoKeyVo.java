619cfcfd611d496d82e8f858df13e7f5cea55a1e
NorthFacing <0haizhu0@gmail.com>
2015-09-14 23:46:22 +0800

    关联关系配置

--- src/main/java/com/bob/biz/demoKey/model/DemoKeyVo.java
+++ src/main/java/com/bob/biz/demoKey/model/DemoKeyVo.java
@@ -1,5 +1,7 @@
 package com.bob.biz.demoKey.model;
 
+import com.bob.biz.demoLock.model.DemoLock;
+
 /**
  * DemoKeyVo
  * 
@@ -8,5 +10,15 @@ package com.bob.biz.demoKey.model;
  * @Date 2015-9-11 14:54:56
  */
 public class DemoKeyVo extends DemoKey {
+
+    private DemoLock demoLock;
+
+    public DemoLock getDemoLock() {
+        return demoLock;
+    }
+
+    public void setDemoLock(DemoLock demoLock) {
+        this.demoLock = demoLock;
+    }
 
 }
--- src/main/java/com/bob/biz/demoLock/model/DemoLockVo.java
+++ src/main/java/com/bob/biz/demoLock/model/DemoLockVo.java
@@ -1,5 +1,9 @@
 package com.bob.biz.demoLock.model;
 
+import java.util.List;
+
+import com.bob.biz.demoKey.model.DemoKey;
+
 /**
  * DemoLockVo
  * 
@@ -8,5 +12,15 @@ package com.bob.biz.demoLock.model;
  * @Date 2015-9-11 14:54:56
  */
 public class DemoLockVo extends DemoLock {
+
+    private List<DemoKey> demoKeyList;
+
+    public List<DemoKey> getDemoKeyList() {
+        return demoKeyList;
+    }
+
+    public void setDemoKeyList(List<DemoKey> demoKeyList) {
+        this.demoKeyList = demoKeyList;
+    }
 
 }
