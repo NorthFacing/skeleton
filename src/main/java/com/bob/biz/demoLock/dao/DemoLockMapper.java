c4d1d7f3082c901658d82830f75ad1b6f19656ef
Bob <0haizhu0@gmail.com>
2015-09-27 01:40:11 +0800

    refactor

---
+++ src/main/java/com/bob/biz/demoLock/dao/DemoLockMapper.java
@@ -0,0 +1,16 @@
+package com.bob.biz.demoLock.dao;
+
+import com.bob.biz.demoLock.model.DemoLock;
+import com.bob.biz.demoLock.model.DemoLockVo;
+import com.bob.core.base.dao.BaseMapper;
+
+/**
+ * DemoLockMapper
+ * 
+ * @since v0.0.1
+ * @author Bob
+ * @Date 2015-9-11 14:54:56
+ */
+public interface DemoLockMapper extends BaseMapper<DemoLock, DemoLockVo> {
+
+}
---
+++ src/main/java/com/bob/modules/organization/dao/OrganizationMapper.java
@@ -0,0 +1,17 @@
+package com.bob.modules.organization.dao;
+
+import com.bob.modules.organization.model.Organization;
+import com.bob.modules.organization.model.OrganizationVo;
+import com.bob.core.base.dao.BaseMapper;
+
+/**
+ * organizationMapper
+ * 
+ * @since v0.0.1
+ * @author Bob
+ * @Date 2015-7-21 15:19:55
+ */
+public interface OrganizationMapper extends BaseMapper<Organization, OrganizationVo> {
+
+    String getCodeByParentId(String parentId);
+}
