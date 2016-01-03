20f0f35cd71cfa4a95c18a56e73fe3ecdf861e08
Bob <0haizhu0@gmail.com>
2016-01-03 22:22:09 +0800

    change web page structure

--- src/main/java/com/bob/modules/sysResource/mapper/SysResourceMapper.java
+++ src/main/java/com/bob/modules/sysResource/mapper/SysResourceMapper.java
@@ -4,6 +4,7 @@ import com.bob.core.base.mapper.BaseMapper;
 import com.bob.core.utils.myBatis.CRUDTemplate;
 import com.bob.modules.sysResource.entity.SysResource;
 import com.bob.modules.sysResource.entity.SysResourceVo;
+
 import org.apache.ibatis.annotations.SelectProvider;
 
 import java.util.List;
