4ede2170ba1f9a99df2a66826c8862a344cc452c
Bob <0haizhu0@gmail.com>
2016-01-18 11:08:38 +0800

    使用lambda表达式重构代码

--- src/main/java/com/bob/core/cache/redis/RedisCacheImpl.java
+++ src/main/java/com/bob/core/cache/redis/RedisCacheImpl.java
@@ -5,9 +5,7 @@ import org.apache.commons.lang.SerializationUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.io.ByteArrayInputStream;
@@ -50,20 +48,17 @@ public class RedisCacheImpl implements Cache {
         if (StringUtils.isBlank(key)) {
             throw new IllegalArgumentException("The cache key：'" + key + "' is invalid.");
         }
-        return redisTemplate.execute(new RedisCallback<T>() {
-            @Override
-            public T doInRedis(RedisConnection connection) throws DataAccessException {
-                byte[] _key = assemblyKey(cacheName, key);
-                if (connection.exists(_key)) {
-                    byte[] _value = connection.get(_key);
-                    try {
-                        return (T) SerializationUtils.deserialize(new ByteArrayInputStream(_value));
-                    } catch (Exception ex) {
-                        logger.error("Read cache object error", ex);
-                    }
+        return redisTemplate.execute((RedisConnection connection) -> {
+            byte[] _key = assemblyKey(cacheName, key);
+            if (connection.exists(_key)) {
+                byte[] _value = connection.get(_key);
+                try {
+                    return (T) SerializationUtils.deserialize(new ByteArrayInputStream(_value));
+                } catch (Exception ex) {
+                    logger.error("Read cache object error", ex);
                 }
-                return null;
             }
+            return null;
         });
     }
 
@@ -90,26 +85,23 @@ public class RedisCacheImpl implements Cache {
         if (value == null) {
             throw new IllegalArgumentException("The cache value：'" + value + "' is invalid.");
         }
-        redisTemplate.execute(new RedisCallback<Void>() {
-            @Override
-            public Void doInRedis(RedisConnection connection) throws DataAccessException {
-                byte[] _key = assemblyKey(cacheName, key);
-                byte[] _value = null;
-                try {
-                    if (!(value instanceof Serializable)) {
-                        throw new IllegalArgumentException(RedisCacheImpl.class.getSimpleName() + " requires a Serializable payload "
-                                + "but received an object of type [" + value.getClass().getName() + "]");
-                    }
-                    _value = SerializationUtils.serialize((Serializable) value);
-                    connection.set(_key, _value);
-                    if (expried > 0) {
-                        connection.expire(_key, expried);
-                    }
-                } catch (Exception ex) {
-                    logger.error("The cache object failed", ex);
+        redisTemplate.execute((RedisConnection connection) -> {
+            byte[] _key = assemblyKey(cacheName, key);
+            byte[] _value = null;
+            try {
+                if (!(value instanceof Serializable)) {
+                    throw new IllegalArgumentException(RedisCacheImpl.class.getSimpleName() + " requires a Serializable payload "
+                            + "but received an object of type [" + value.getClass().getName() + "]");
                 }
-                return null;
+                _value = SerializationUtils.serialize((Serializable) value);
+                connection.set(_key, _value);
+                if (expried > 0) {
+                    connection.expire(_key, expried);
+                }
+            } catch (Exception ex) {
+                logger.error("The cache object failed", ex);
             }
+            return null;
         });
     }
 
@@ -120,12 +112,10 @@ public class RedisCacheImpl implements Cache {
 
     @Override
     public boolean del(final String cacheName, final String key) {
-        return redisTemplate.execute(new RedisCallback<Boolean>() {
-            @Override
-            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
-                return connection.del(assemblyKey(cacheName, key)) > 0;
-            }
-        });
+        return redisTemplate.execute(
+                (RedisConnection connection) ->
+                        (connection.del(assemblyKey(cacheName, key)) > 0)
+        );
     }
 
     @Override
@@ -143,14 +133,12 @@ public class RedisCacheImpl implements Cache {
         if (StringUtils.isBlank(cacheName)) {
             throw new IllegalArgumentException("The cache name：'" + cacheName + "' is invalid.");
         }
-        return redisTemplate.execute(new RedisCallback<Set<byte[]>>() {
-            @Override
-            public Set<byte[]> doInRedis(RedisConnection connection) throws DataAccessException {
-                byte[] _key_pattern = redisTemplate.getStringSerializer().serialize(cacheName + CONNECTOR + "*");
-                Set<byte[]> _keyset = connection.keys(_key_pattern);
-                return _keyset;
-            }
-        });
+        return redisTemplate.execute((RedisConnection connection) -> {
+                    byte[] _key_pattern = redisTemplate.getStringSerializer().serialize(cacheName + CONNECTOR + "*");
+                    Set<byte[]> _keyset = connection.keys(_key_pattern);
+                    return _keyset;
+                }
+        );
     }
 
     @Override
@@ -158,24 +146,21 @@ public class RedisCacheImpl implements Cache {
         if (StringUtils.isBlank(cacheName)) {
             throw new IllegalArgumentException("The cache name：'" + cacheName + "' is invalid.");
         }
-        return redisTemplate.execute(new RedisCallback<Integer>() {
-            @Override
-            public Integer doInRedis(RedisConnection connection) throws DataAccessException {
-                StringBuilder builder = new StringBuilder();
-                if (StringUtils.isNotBlank(prefix)) {
-                    builder.append(prefix).append(CONNECTOR);
-                }
-                builder.append(cacheName).append(CONNECTOR).append("*");
-
-                byte[] _key_pattern = redisTemplate.getStringSerializer().serialize(builder.toString());
-                Set<byte[]> _keyset = connection.keys(_key_pattern);
-                byte[][] _keys = new byte[_keyset.size()][];
-                int i = 0;
-                for (byte[] _key : _keyset) {
-                    _keys[i++] = _key;
-                }
-                return connection.del(_keys).intValue();
+        return redisTemplate.execute((RedisConnection connection) -> {
+            StringBuilder builder = new StringBuilder();
+            if (StringUtils.isNotBlank(prefix)) {
+                builder.append(prefix).append(CONNECTOR);
             }
+            builder.append(cacheName).append(CONNECTOR).append("*");
+
+            byte[] _key_pattern = redisTemplate.getStringSerializer().serialize(builder.toString());
+            Set<byte[]> _keyset = connection.keys(_key_pattern);
+            byte[][] _keys = new byte[_keyset.size()][];
+            int i = 0;
+            for (byte[] _key : _keyset) {
+                _keys[i++] = _key;
+            }
+            return connection.del(_keys).intValue();
         });
     }
 
