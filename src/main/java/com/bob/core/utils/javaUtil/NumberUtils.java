65767b7e4f2d16cf8b2cb735d99a4f727175f66b
Bob <0haizhu0@gmail.com>
2016-01-23 19:16:55 +0800

    数字工具类

--- src/main/java/com/bob/core/utils/javaUtil/NumberUtils.java
+++ src/main/java/com/bob/core/utils/javaUtil/NumberUtils.java
@@ -5,7 +5,7 @@ package com.bob.core.utils.javaUtil;
  */
 public class NumberUtils {
 
-    public static void test(int number) {
+    public static String test(int number) {
         String[] cnZhNum = {"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
         String[] unit = new String[]{"元", "拾", "佰", "仟", "万", "拾", "佰", "仟", "亿"};
 
@@ -16,17 +16,50 @@ public class NumberUtils {
             String index = String.valueOf(strNum.charAt(i));
             sb = sb.append(cnZhNum[Integer.parseInt(index)]);
         }
-        String sss = String.valueOf(sb);
+
         int i = 0;
-        for (int j = sss.length(); j > 0; j--) {
+        for (int j = strNum.length(); j > 0; j--) {
             sb = sb.insert(j, unit[i++]);
         }
-        System.out.println(sb);
+        return sb.toString();
+    }
+
+    public static String week(int number) {
+        String strWeek;
+        switch (number) {
+            case 1:
+                strWeek = "星期一";
+                break;
+            case 2:
+                strWeek = "星期二";
+                break;
+            case 3:
+                strWeek = "星期三";
+                break;
+            case 4:
+                strWeek = "星期四";
+                break;
+            case 5:
+                strWeek = "星期五";
+                break;
+            case 6:
+                strWeek = "星期六";
+                break;
+            case 7:
+                strWeek = "星期天";
+                break;
+            default:
+                strWeek = "数据错误！";
+                break;
+        }
+        return strWeek;
     }
 
     public static void main(String[] args) {
-        NumberUtils d = new NumberUtils();
-        d.test(9847337);
+        System.out.println(test(9847337));
+        System.out.println(week(2));
+        System.out.println(week(7));
+        System.out.println(week(9));
     }
 
 }
