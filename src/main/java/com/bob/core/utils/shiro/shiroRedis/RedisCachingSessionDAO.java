8b721c61eecbe91ea23dd0cb4dcc3915eb2afc12
Bob <0haizhu0@gmail.com>
2016-09-12 23:12:42 +0800

    shiro架构调整：使用自定義session

--- src/main/java/com/bob/core/utils/shiro/shiroRedis/RedisCachingSessionDAO.java
+++ src/main/java/com/bob/core/utils/shiro/shiroRedis/RedisCachingSessionDAO.java
@@ -1,101 +1,107 @@
-//package com.bob.core.utils.shiro.shiroRedis;
-//
-//import com.bob.core.contants.BizConfig;
-//import org.apache.shiro.session.Session;
-//import org.apache.shiro.session.UnknownSessionException;
-//import org.apache.shiro.session.mgt.ValidatingSession;
-//import org.apache.shiro.session.mgt.eis.CachingSessionDAO;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//import java.io.Serializable;
-//
-///**
-// * 使用RedisSessionDAO替换shiro默认session管理
-// *
-// * @Note 继承AbstractSessionDAO实现的时候，doReadSession方法调用过于频繁，所以改为通过集成CachingSessionDAO来实现。
-// * @Warn 注意，本地缓存通过EhCache实现，失效时间一定要远小于Redis失效时间，
-// * 这样本地失效后，会访问Redis读取，并重新设置Redis上会话数据的过期时间。
-// * <p>
-// * TODO：为什么需要这个类？有什么作用？
-// */
-//public class RedisCachingSessionDAO extends CachingSessionDAO {
-//
-//  private static Logger logger = LoggerFactory.getLogger(RedisCachingSessionDAO.class);
-//
-//  private String sessionPrefix = "shiro_session"; // 作为默认值
-//
-//  @Override
-//  protected void doUpdate(Session session) {
-//    if (session == null || session.getId() == null) {
-//      logger.error("session or session id is null");
-//      return;
-//    }
-//    if (session instanceof ValidatingSession && !((ValidatingSession) session).isValid()) {
-//      return; //如果会话过期/停止 没必要再更新了
-//    }
-////    saveSession(session);
-//  }
-//
-//  @Override
-//  protected void doDelete(Session session) {
-//    if (session == null || session.getId() == null) {
-//      logger.error("session or session id is null");
-//      return;
-//    }
-////    redisImpl.del(getKey(session.getId()));
-//  }
-//
-//  @Override
-//  protected Serializable doCreate(Session session) {
-//    Serializable sessionId = super.generateSessionId(session);
-//    super.assignSessionId(session, sessionId);
-//    saveSession(session);
-//    return sessionId;
-//  }
-//
-//  @Override
-//  protected Session doReadSession(Serializable sessionId) {
-//    if (sessionId == null) {
-//      logger.error("session id is null");
-//      return null;
-//    }
-////    Object s = redisImpl.get(getKey(sessionId));
-////    if (s == null) {
-////      return null;
-////    } else {
-////      return (Session) s;
-////    }
-//    return null;
-//  }
-//
-//  /**
-//   * save session
-//   *
-//   * @param session session
-//   * @throws UnknownSessionException UnknownSessionException
-//   */
-//  private void saveSession(Session session) throws UnknownSessionException {
-//    if (session == null || session.getId() == null) {
-//      logger.error("session or session id is null");
-//      return;
-//    }
-//    String key = getKey(session.getId());
-//    long timeOut = BizConfig.sessionTimeOut;
-//    session.setTimeout(timeOut * 1000);
-////    redisImpl.set(key, (ShiroSession) session, timeOut);
-//  }
-//
-//  /**
-//   * 获得byte[]型的key
-//   *
-//   * @param sessionId sessionId
-//   * @return
-//   */
-//  private String getKey(Serializable sessionId) {
-//    String preKey = this.sessionPrefix + ":" + sessionId;
-//    return preKey;
-//  }
-//
-//
-//}
+package com.bob.core.utils.shiro.shiroRedis;
+
+import com.bob.core.utils.shiro.session.ShiroSession;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.mgt.ValidatingSession;
+import org.apache.shiro.session.mgt.eis.CachingSessionDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class RedisCachingSessionDAO extends CachingSessionDAO {
+
+  private static final Logger logger = LoggerFactory.getLogger(RedisCachingSessionDAO.class);
+
+  /**
+   * 根据会话ID获取会话
+   *
+   * @param sessionId 会话ID
+   * @return ShiroSession
+   */
+  @Override
+  protected Session doReadSession(Serializable sessionId) {
+    logger.debug("begin doReadSession {} ", sessionId);
+    Session session = null;
+    try {
+      session = super.getCachedSession(sessionId);
+      if (session != null) {
+//        sessionRepository.refreshSession(sessionId);
+        logger.info("sessionId {} name {} 被读取", sessionId, session.getClass().getName());
+      }
+    } catch (Exception e) {
+      logger.warn("读取Session失败", e);
+    }
+    return session;
+  }
+
+  /**
+   * 如DefaultSessionManager在创建完session后会调用该方法；
+   * 如保存到关系数据库/文件系统/NoSQL数据库；即可以实现会话的持久化；
+   * 返回会话ID；主要此处返回的ID.equals(session.getId())；
+   */
+  @Override
+  protected Serializable doCreate(Session session) {
+    // 创建一个Id并设置给Session
+    Serializable sessionId = super.generateSessionId(session);
+    super.assignSessionId(session, sessionId);
+    try {
+      super.cache(session, session.getId());
+      logger.info("sessionId {} name {} 被创建", sessionId, session.getClass().getName());
+    } catch (Exception e) {
+      logger.warn("创建Session失败", e);
+    }
+    return sessionId;
+  }
+
+  /**
+   * 更新会话；如更新会话最后访问时间/停止会话/设置超时时间/设置移除属性等会调用
+   */
+  @Override
+  protected void doUpdate(Session session) {
+    //如果会话过期/停止 没必要再更新了
+    try {
+      if (session instanceof ValidatingSession && !((ValidatingSession) session).isValid()) {
+        return;
+      }
+    } catch (Exception e) {
+      logger.error("ValidatingSession error");
+    }
+    try {
+      if (session instanceof ShiroSession) {
+        // 如果没有主要字段(除lastAccessTime以外其他字段)发生改变
+        ShiroSession ss = (ShiroSession) session;
+        if (!ss.isChanged()) {
+          return;
+        }
+        ss.setChanged(false);
+        ss.setLastAccessTime(new Date());
+        super.update(session);
+        //发送广播
+//                jedisUtil.publish("shiro.session.uncache", session.getId());
+        logger.debug("sessionId {} name {} 被更新", session.getId(), session.getClass().getName());
+      } else {
+        logger.debug("sessionId {} name {} 更新失败", session.getId(), session.getClass().getName());
+      }
+    } catch (Exception e) {
+      logger.warn("更新Session失败", e);
+    }
+  }
+
+  /**
+   * 删除会话；当会话过期/会话停止（如用户退出时）会调用
+   */
+  @Override
+  public void doDelete(Session session) {
+    logger.debug("begin doDelete {} ", session);
+    try {
+      super.delete(session);
+      logger.debug("shiro session id {} 被删除", session.getId());
+    } catch (Exception e) {
+      logger.warn("删除Session失败", e);
+    }
+  }
+
+
+}
