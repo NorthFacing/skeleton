9582006b9d6b34e3353daa1b3ffbc034b0bf0a08
Bob <0haizhu0@gmail.com>
2017-01-10 11:12:42 +0800

    SQL

--- src/test/temp/01全部.sql
+++ src/test/temp/01全部.sql
@@ -1,22 +1,52 @@
+-- 间隔天
+SELECT
+  date_sub(curdate(), INTERVAL 1 DAY)                              totaldate,
+  NULL                                                             mdcode,
+  NULL                                                             lineid,
+  NULL                                                             devavg,
+  cast((max(T.day) - min(T.day)) / count(1) AS DECIMAL(10, 0)) + 1 useavg,
+  NULL                                                             7dproportion
+FROM (
+       SELECT
+         DISTINCT to_days(o.cdtime) day
+       FROM operaterecord_data_80a0 o
+         -- 日志和用户关联
+         LEFT JOIN (
+                     SELECT DISTINCT
+                       ud.did,
+                       ud.uid
+                     FROM ia_user_dev ud
+                       -- 设备和用户关联
+                       LEFT JOIN ia_appusers u
+                         ON ud.uid = u.id
+                     WHERE ud.deleted = 0
+                           AND u.deleted = 0
+                     GROUP BY ud.did
+                   ) A
+           ON o.machineid = A.did
+       WHERE o.deleted = 0
+             AND DATE_FORMAT(o.cdtime, '%Y-%m-%d') < curdate()
+             AND A.uid IS NOT NULL
+       GROUP BY to_days(o.cdtime)
+     ) T;
+
 -- 7日占比
 
 SELECT
-
   T.totaldate,
-  sum(
-      CASE WHEN T.useavg <= 7
-        THEN 1
-      ELSE 0 END
-  )                                       part,
-  count(1)                                total,
-  cast(sum(
-           CASE WHEN T.useavg <= 7
-             THEN 1
-           ELSE 0 END
-       ) / count(1) AS DECIMAL(10, 2)) AS 7dproportion
+  NULL        mdcode,
+  NULL        lineid,
+  NULL        devavg,
+  NULL        useavg,
+  concat(cast(sum(
+                  CASE WHEN T.useavg <= 7
+                    THEN 1
+                  ELSE 0 END
+              ) * 100 / count(1) AS DECIMAL(10, 1)),
+         '%') 7dproportion
 FROM
   (
-    -- 间隔天数
+    -- 按照产品线分组的间隔天数
     SELECT
       date_sub(curdate(), INTERVAL 1 DAY)                                                    totaldate,
       B.pid                                                                                  lineId,
@@ -52,12 +82,12 @@ FROM
       LEFT JOIN (
                   -- 每个产品线不同日期的总天数
                   SELECT
-                    count(M.day) days,
+                    COUNT(M.d) days,
                     M.lineId
                   FROM (
                          -- 每个产品线同一天的记录只取一条
                          SELECT DISTINCT
-                           to_days(o2.cdtime) day,
+                           to_days(o2.cdtime) d,
                            im.pid             lineId
                          FROM operaterecord_data_80a0 o2
                            LEFT JOIN ia_productmodel im
