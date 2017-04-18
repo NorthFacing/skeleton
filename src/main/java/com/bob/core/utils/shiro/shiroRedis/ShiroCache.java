b401018ffbc07b159a06b798dca03e1dccc40d29
Bob <0haizhu0@gmail.com>
2017-04-19 02:28:13 +0800

    實現

--- src/main/java/com/bob/core/utils/shiro/shiroRedis/ShiroCache.java
+++ src/main/java/com/bob/core/utils/shiro/shiroRedis/ShiroCache.java
@@ -1,19 +1,13 @@
 package com.bob.core.utils.shiro.shiroRedis;
 
-import com.bob.core.utils.shiro.session.ShiroSession;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.shiro.cache.Cache;
 import org.apache.shiro.cache.CacheException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.data.convert.ReadingConverter;
-import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -26,83 +20,69 @@ public class ShiroCache<K, V> implements Cache<K, V> {
 
   private Logger logger = LoggerFactory.getLogger(ShiroCache.class);
 
-  @Autowired
-  private RedisTemplate<String, byte[]> redisTemplate;
+  private RedisTemplate<K, V> redisTemplate;
 
   @Override
   public V get(K key) throws CacheException {
-    logger.debug("[ShiroCache] 根据key从Redis中获取shiro对象： key = {}", key);
-    return null;
+    if (!(key instanceof String))
+      throw new CacheException("[ShiroCache] key 值类型错误！");
+    V value = redisTemplate.opsForValue().get(assembleKey((String) key));
+    logger.debug("[ShiroCache] 根据key从Redis中获取shiro对象： {} = {}", key, value);
+    return value;
   }
 
   @Override
   public V put(K key, V value) throws CacheException {
+    if (!(key instanceof String))
+      throw new CacheException("[ShiroCache] key 值类型错误！");
+    redisTemplate.opsForValue().set((K) assembleKey((String) key), value);
     logger.debug("[ShiroCache] 添加shiro对象到redis： {} = {}", key, value);
-    return null;
+    return value;
   }
 
   @Override
   public V remove(K key) throws CacheException {
+    if (!(key instanceof String))
+      throw new CacheException("[ShiroCache] key 值类型错误！");
     logger.debug("[ShiroCache] 从redis中删除shiro对象： key = {}", key);
-    return null;
+    V value = redisTemplate.opsForValue().get(assembleKey((String) key));
+    redisTemplate.delete(key);
+    return value;
   }
 
   @Override
   public void clear() throws CacheException {
     logger.debug("[ShiroCache] 从redis中清空shiro所有对象");
+    redisTemplate.delete((K) assembleKey("*"));
   }
 
   @Override
   public int size() {
     logger.debug("[ShiroCache] 获取dbSize");
-//    logger.debug("[ShiroCache] 获取dbSize：{}", size);
-    return 0;
+    int size = redisTemplate.keys((K) assembleKey("*")).size();
+    logger.debug("[ShiroCache] 获取dbSize：{}", size);
+    return size;
   }
 
   @Override
   public Set<K> keys() {
     logger.debug("[ShiroCache] 从redis缓存shiro对象的keys");
-//    logger.debug("[ShiroCache] 从redis缓存shiro对象的keys：共计 {} 个", kKeys.size());
-    return null;
+    Set<K> keys = redisTemplate.keys((K) assembleKey("*"));
+    logger.debug("[ShiroCache] 从redis缓存shiro对象的keys：共计 {} 个", keys.size());
+    return keys;
   }
 
   @Override
   public Collection<V> values() {
     logger.debug("[ShiroCache] redis缓存shiro对象的values");
-//    logger.debug("[ShiroCache] redis缓存shiro对象的values：共计 {} 个", values.size());
-    return null;
+    Set<K> keys = redisTemplate.keys((K) assembleKey("*"));
+    List<V> values = redisTemplate.opsForValue().multiGet(keys);
+    logger.debug("[ShiroCache] redis缓存shiro对象的values：共计 {} 个", values.size());
+    return values;
   }
 
-}
-
-@WritingConverter
-class ShiroSessionToBytesConverter implements Converter<ShiroSession, byte[]> {
-
-  private final Jackson2JsonRedisSerializer<ShiroSession> serializer;
-
-  public ShiroSessionToBytesConverter() {
-    serializer = new Jackson2JsonRedisSerializer<>(ShiroSession.class);
-    serializer.setObjectMapper(new ObjectMapper());
+  private String assembleKey(String key) {
+    return new StringBuffer("shiro-").append(key).toString();
   }
 
-  @Override
-  public byte[] convert(ShiroSession value) {
-    return serializer.serialize(value);
-  }
-}
-
-@ReadingConverter
-class BytesToShiroSessionConverter implements Converter<byte[], ShiroSession> {
-
-  private final Jackson2JsonRedisSerializer<ShiroSession> serializer;
-
-  public BytesToShiroSessionConverter() {
-    serializer = new Jackson2JsonRedisSerializer<>(ShiroSession.class);
-    serializer.setObjectMapper(new ObjectMapper());
-  }
-
-  @Override
-  public ShiroSession convert(byte[] value) {
-    return serializer.deserialize(value);
-  }
 }
